03909e7e8805978c5ee905affbfd6b5e55ba4556
Arman Jasuja <arman_jasuja@yahoo.com>
2024-12-24 10:53:24 +0400

    Part 2 Solution

--- 2024/actual_solution/1.py
+++ 2024/actual_solution/1.py
@@ -8,9 +8,13 @@ def main():
     a_1.sort()
     a_2.sort()
     distance_sum = 0
+    sim_score = 0
     for i in range(1000):
         distance_sum += abs(a_1[i] - a_2[i])
+        sim_score += a_1[i] * a_2.count(a_1[i])
     print(distance_sum)
+    print(sim_score)
+    
     
     
 if __name__ == "__main__":
